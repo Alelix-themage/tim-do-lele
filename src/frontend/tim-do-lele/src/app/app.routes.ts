43998fecf137b49693197a33fb164f8844595558
Alelix-themage <alessandrojunior864@gmail.com>
2024-10-08 19:09:22 -0300

    Inclua o component de admin

--- src/frontend/tim-do-lele/src/app/app.routes.ts
+++ src/frontend/tim-do-lele/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        component: AppComponent,
+        title: "Home"
+    },
+
+    {
+        path: "admin",
+        component: AdminComponent,
+        title: "Admin",
+    },
+];
