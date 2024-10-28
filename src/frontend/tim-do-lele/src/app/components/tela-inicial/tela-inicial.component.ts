e00162da8e329f2aa369283b4e9776146a2367ee
Aleliks <72809824+Alelix-themage@users.noreply.github.com>
2024-10-28 12:54:29 -0300

    Inclua o modelo de objeto do lanche

---
+++ src/frontend/tim-do-lele/src/app/components/tela-inicial/tela-inicial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicial',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-inicial.component.html',
+  styleUrl: './tela-inicial.component.scss'
+})
+export class TelaInicialComponent {
+
+}
