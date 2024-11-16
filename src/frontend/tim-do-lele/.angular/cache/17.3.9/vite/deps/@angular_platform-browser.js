07de0d7e08b92dc3f72cccd98fd17d31cc60dd46
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2024-11-16 11:07:04 -0300

    cart-implementation

--- src/frontend/tim-do-lele/.angular/cache/17.3.9/vite/deps/@angular_platform-browser.js
+++ src/frontend/tim-do-lele/.angular/cache/17.3.9/vite/deps/@angular_platform-browser.js
@@ -35,15 +35,15 @@ import {
   provideProtractorTestingSupport,
   withHttpTransferCacheOptions,
   withNoHttpTransferCache
-} from "./chunk-N5UNED6L.js";
-import "./chunk-3YRFB4LK.js";
+} from "./chunk-NXGQDR2A.js";
+import "./chunk-XYEN4V2U.js";
 import {
   getDOM
-} from "./chunk-S43UIT6S.js";
-import "./chunk-3QEE4RPL.js";
-import "./chunk-JND6LT5A.js";
-import "./chunk-664N5FMB.js";
-import "./chunk-532FTKWE.js";
+} from "./chunk-T3KGFML5.js";
+import "./chunk-HUFKUHAV.js";
+import "./chunk-AYIOQOC7.js";
+import "./chunk-YLULMU56.js";
+import "./chunk-B2OBZUDO.js";
 import "./chunk-J4B6MK7R.js";
 export {
   BrowserModule,
--- src/frontend/tim-do-lele/src/app/components/navbar/navbar.component.ts
+++ src/frontend/tim-do-lele/src/app/components/navbar/navbar.component.ts
@@ -10,10 +10,17 @@ import { CommonModule } from '@angular/common';
 })
 export class NavbarComponent {
   @ViewChild('searchForm') searchForm!: ElementRef;
+  @ViewChild('cart') cart!: ElementRef;
   @Output() cartClicked = new EventEmitter<void>(); // Evento para o carrinho
+
+  quantity: number = 1; // A quantidade começa em 1
 
   toggleSearchForm() {
     this.searchForm.nativeElement.classList.toggle('active');
+  }
+
+  toggleCart() {
+    this.cart.nativeElement.classList.toggle('active');
   }
 
   onCartClick() {
@@ -23,5 +30,17 @@ export class NavbarComponent {
   toggleMenu() {
     const nav = document.querySelector('.nav');
     nav?.classList.toggle('active');
+  }
+
+  // Método para diminuir a quantidade
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--; // Só diminui se a quantidade for maior que 1
+    }
+  }
+
+  // Método para aumentar a quantidade
+  increaseQuantity() {
+    this.quantity++; // Aumenta a quantidade
   }
 }
