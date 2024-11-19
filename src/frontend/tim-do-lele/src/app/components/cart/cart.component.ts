657c4f3cccfc6872c831855526e4c1e552d7c12a
Aleliks <72809824+Alelix-themage@users.noreply.github.com>
2024-11-19 17:25:36 -0300

    Inclua o CommonModule no cart

--- src/frontend/tim-do-lele/src/app/components/cart/cart.component.ts
+++ src/frontend/tim-do-lele/src/app/components/cart/cart.component.ts
@@ -1,10 +1,12 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { CartService } from 'app/service/cart.service';
 import { Food } from '../Food.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
