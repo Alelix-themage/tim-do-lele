82d9a1c087ed13f88968e43e098c130b3d121afc
Felipe Ferreira <felipe.fr.almeida@gmail.com>
2024-11-20 21:17:29 -0300

    fixes

--- src/frontend/tim-do-lele/src/app/components/admin/admin.component.ts
+++ src/frontend/tim-do-lele/src/app/components/admin/admin.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminNavbarComponent } from '../admin-navbar/admin-navbar.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AdminNavbarComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
