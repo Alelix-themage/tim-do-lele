09f4391754b27c936fbd9b040a4222449888e8e4
Aleliks <72809824+Alelix-themage@users.noreply.github.com>
2024-12-03 10:28:11 -0300

    Inclua a consulta de pedidos

--- src/src/backend/query_banco/consulta_pedidos.js
+++ src/src/backend/query_banco/consulta_pedidos.js
@@ -6,31 +6,37 @@ function ConsultarPedidos() {
 
     return new Promise((resolve, reject) => {
         db.all(`
-            SELECT
-                P.ID AS PedidoID,
-                U.NOME AS NomeUsuario,
-                P.DATA AS DataPedido,
-                P.HORARIO_RESERVA AS HorarioReserva,
-                P.STATUS AS StatusPedido,
-                CASE
-                    WHEN PI.TIPO = 'Lanche' THEN L.NOME
-                    WHEN PI.TIPO = 'Bebida' THEN B.NOME
-                    ELSE NULL
-                END AS NomeItem,
-                PI.QUANTIDADE AS Quantidade,
-                GROUP_CONCAT(M.NOME, ', ') AS Molhos
-            FROM
-                PEDIDOS P
-                JOIN USERS U ON P.USER_ID = U.ID
-                JOIN PEDIDOS_ITENS PI ON P.ID = PI.PEDIDO_ID
-                LEFT JOIN LANCHES L ON PI.ITEM_ID = L.ID AND PI.TIPO = 'Lanche'
-                LEFT JOIN BEBIDAS B ON PI.ITEM_ID = B.ID AND PI.TIPO = 'Bebida'
-                LEFT JOIN PEDIDOS_ITENS MPI ON P.ID = MPI.PEDIDO_ID AND MPI.TIPO = 'Molho'
-                LEFT JOIN MOLHOS M ON MPI.ITEM_ID = M.ID
-            GROUP BY
-                P.ID, U.NOME, P.DATA, P.HORARIO_RESERVA, P.STATUS, PI.ID
-            ORDER BY
-                P.ID, PI.ID;
+                SELECT
+            P.ID AS PedidoID,
+            U.NOME AS NomeUsuario,
+            P.DATA AS DataPedido,
+            P.HORARIO_RESERVA AS HorarioReserva,
+            P.STATUS AS StatusPedido,
+            CASE
+                WHEN PI.TIPO = 'Lanche' THEN L.NOME
+                WHEN PI.TIPO = 'Bebida' THEN B.NOME
+                ELSE NULL
+            END AS NomeItem,
+            PI.QUANTIDADE AS Quantidade,
+            CASE
+                WHEN PI.TIPO = 'Lanche' THEN (
+                    SELECT GROUP_CONCAT(M2.NOME, ', ')
+                    FROM PEDIDOS_ITENS PI2
+                    JOIN MOLHOS M2 ON PI2.ITEM_ID = M2.ID
+                    WHERE PI2.PEDIDO_ID = P.ID AND PI2.TIPO = 'Molho'
+                )
+                ELSE NULL
+            END AS Molhos
+        FROM
+            PEDIDOS P
+            JOIN USERS U ON P.USER_ID = U.ID
+            JOIN PEDIDOS_ITENS PI ON P.ID = PI.PEDIDO_ID
+            LEFT JOIN LANCHES L ON PI.ITEM_ID = L.ID AND PI.TIPO = 'Lanche'
+            LEFT JOIN BEBIDAS B ON PI.ITEM_ID = B.ID AND PI.TIPO = 'Bebida'
+        WHERE
+            PI.TIPO != 'Molho'
+        ORDER BY
+            P.ID, PI.ID;
         `, (error, rows) => {  
             if (error) {
                 console.error("Erro ao consultar a tabela PEDIDOS:", error.message);
